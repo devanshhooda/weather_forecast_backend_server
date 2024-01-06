f02c64e2d9dc8f4c1f2e5d46aba4b5845878f106
devanshhooda <devanshstan17@gmail.com>
2024-01-06 16:27:31 +0530

    Slight change in schema and packages

--- models/location.js
+++ models/location.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
+  name: { type: String, required: true, unique: true},
+  latitude: { type: Number, required: true},
+  longitude: { type: Number, required: true}
 });
 
 const Location = mongoose.model('Location', locationSchema);
